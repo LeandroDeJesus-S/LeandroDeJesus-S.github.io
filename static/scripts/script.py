a917972d325515a942e9202be8a372c784ea53a7
LeandroDeJesus-S <jstleandro@gmail.com>
2024-10-08 04:54:33 -0300

    feat: remake all the site design

--- static/scripts/script.py
+++ static/scripts/script.py
@@ -1,40 +1,28 @@
-from browser import document, aio, window
+from browser import document, bind
 from urllib.parse import quote
 
+form = document["contact-form"]
+form_style = """
+    background-color: #2a3f54;
+    border-radius: 200px;
+    padding: 1.4%;
+    width: 50%;
+    display: flex;
+    justify-content: center;
+"""
 
-async def main():
-    inp = document['contact-input']
-    ctt_div = document['contact-input-div']
-    ctt_btn = document['contact-button']
-
-    inp.bind(
-        'focus', 
-        lambda e: ctt_div.set_style(
-            "border-radius: 20px; width: 20rem; border: 3px solid #c89fa3; transition: border-color 0.3s;"
-        )
-    )
-    inp.bind(
-        'blur', 
-        lambda e: ctt_div.set_style(
-            "border-radius: 20px; width: 20rem; border: 3px solid transparent; transition: border-color 0.3s;"
-        )
-    )
-    
-    def on_keyup(e):
-        text = quote(str(inp.value).strip())
-        ctt_btn.setAttribute('href', f'https://wa.me/5527988724130?text={text}')
-
-    inp.bind('keyup', on_keyup)
-
-    social = document['social']
-    def on_resize(e):
-        if social.width > 100:
-            social.setAttribute('class', 'col-md-2 d-flex justify-content-center gap-3 g-0')
-        else:
-            social.setAttribute('class', 'col-md-2 d-flex flex-column justify-content-end gap-3 g-0')
-        
-    on_resize(None)
-    window.bind('resize', on_resize)
+@bind('#contact-input', 'focus')
+def on_focus_contact_input(e):
+    form.set_style(form_style + "border: 3px solid #3a6a8b")
 
 
-aio.run(main())
+@bind('#contact-input', 'blur')
+def on_blur_contact_input(e):
+    form.set_style(form_style)
+
+
+@bind('#contact-input', 'keyup')
+def on_keyup(e):
+    text = quote(str(e.target.value).strip())
+    if text:
+        document['contact-button'].setAttribute('href', f'https://wa.me/5527988724130?text={text}')
